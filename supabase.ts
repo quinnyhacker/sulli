b4f601391c08ceec325d057f0828b40663a874e9
Quinn <quinnpjones01@gmail.com>
2026-05-26 21:12:41 -0500

    fix package versions

--- supabase.ts
+++ supabase.ts
@@ -1,6 +1,14 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://qqfsawsritpfjneuahgc.supabase.co'
 const supabaseKey = 'sb_publishable_q2qciSQiLbzH97lr1Kg1FQ__1z8PY0u'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
